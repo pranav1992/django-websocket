df8485bf0a776910c5761a94657120ee646819e9
pranav chourasiya <pranavchourasiya@pranavs-MacBook-Air.local>
2025-09-18 11:52:03 +0530

    first commit

---
+++ djangosockets/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+echo "Running Django migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+exec "$@"
